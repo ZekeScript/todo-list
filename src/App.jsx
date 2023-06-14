eb3fa81fb702cf250bcfa33127af2633035a0b10
ZekeScript <estiga27@gmail.com>
2023-06-14 17:31:11 -0300

    add task feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInput = (e = document.getElementById('task-input')) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleButton = () => {
+    setTasks([...tasks, inputValue])
+  }
 
   return (
     <>
       <h1>ToDo List</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      {tasks.map((task, index) => {
+        return (
+          <div className='task card' key={index}>
+            {task}
+          </div>
+        )
+      })}
+      <section className='input-field'>
+        <input id='task-input' placeholder='New Task' value={inputValue} onChange={handleInput} />
+        <button onClick={handleButton}>+</button>
+      </section>
     </>
   )
 }

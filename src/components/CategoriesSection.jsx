72bb22d56c4803f1d0231b4cac2f7ef59469e1de
ZekeScript <estiga27@gmail.com>
2023-06-24 23:30:21 -0300

    add animated bar

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -1,15 +1,33 @@
 export const CategoriesSection = ({ index, tasks, categorie }) => (
   <div className='task-categorie' key={index}>
-    <div className='task-count'>{tasks.filter(task => task.categorie === categorie).length + ' Tasks'}</div>
+    <div className='task-categorie-count'>
+      {tasks.filter(task => task.categorie === categorie).length + ' Tasks'}
+    </div>
     <div>
+      {categorie}
       {categorie === 'Business'
         ? (
-          <svg style={{ marginRight: '10px' }} xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px' fill='#EB06FF'><path d='M0 0h24v24H0z' fill='none' /><path d='M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z' /></svg>
+          <div className='task-categorie-bar'>
+            <div style={{
+              backgroundColor: '#EB06FF',
+              height: '100%',
+              width: `${tasks.filter(task => task.categorie === categorie).length * 100 / tasks.length}%`,
+              transition: '300ms'
+            }}
+            />
+          </div>
           )
         : (
-          <svg style={{ marginRight: '10px' }} xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px' fill='#227BFF'><path d='M0 0h24v24H0z' fill='none' /><path d='M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z' /></svg>
+          <div className='task-categorie-bar'>
+            <div style={{
+              backgroundColor: '#227BFF',
+              height: '100%',
+              width: `${tasks.filter(task => task.categorie === categorie).length * 100 / tasks.length}%`,
+              transition: '300ms'
+            }}
+            />
+          </div>
           )}
-      {categorie}
     </div>
   </div>
 )
